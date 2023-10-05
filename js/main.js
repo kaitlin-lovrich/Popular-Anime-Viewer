cdf9fe3503295b28c2856645d1bfd835ebcd1de6
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-05 22:11:25 +0000

    Populated home page with different genres and made the page horizontally scrollable #25 #26 #27 #28 #29 #30 #31 #32 #33 #34 #35

--- js/main.js
+++ js/main.js
@@ -20,29 +20,80 @@ function populateRow(urlQuery, genreOrCategoryName) {
   xhr.open('GET', urlQuery);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // console.log('xhr.status:', xhr.status);
     // console.log('xhr.response:', xhr.response);
     const $h2 = document.createElement('h2');
     $h2.textContent = genreOrCategoryName.toUpperCase();
+    const $div = document.createElement('div');
+    $div.className = 'relative';
+    const $i = document.createElement('i');
+    $i.className = 'fa-solid fa-chevron-left';
     const $ul = document.createElement('ul');
     $ul.className = 'row';
-    $titles.appendChild($h2);
+    const $i2 = document.createElement('i');
+    $i2.className = 'fa-solid fa-chevron-right';
 
+    $titles.appendChild($h2);
+    $titles.appendChild($div);
+    $div.appendChild($i);
     for (const title of xhr.response.data) {
       const $titleImage = renderTitles(title);
-      // console.log('$titleImage', $titleImage);
       $ul.appendChild($titleImage);
     }
-    $titles.appendChild($ul);
+    $div.appendChild($ul);
+    $div.appendChild($i2);
   });
   xhr.send();
 }
 
-populateRow('https://api.jikan.moe/v4/anime?order_by=popularity&genres=fantasy', 'Fantasy');
 populateRow('https://api.jikan.moe/v4/anime?order_by=popularity', 'Popular');
 
-// Position arrow so it is relative to the edge of the screen, not the element? make it so it appears IF the ul row is being hovered
+function getGenreId() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.jikan.moe/v4/genres/anime?filter=genres');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', async function () {
+    function delay(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    const createUser = id => {
+      return fetch(
+        `https://api.jikan.moe/v4/anime?order_by=popularity&genres=${id}`
+      );
+    };
+    async function runPromises() {
+      for (const genre of xhr.response.data) {
+        // console.log('genre', genre);
+        const response = await createUser(genre.mal_id);
+        const genreID = await response.json();
 
-// How to make elements scroll off the screen?
+        const $h2 = document.createElement('h2');
+        $h2.textContent = genre.name.toUpperCase();
+        const $div = document.createElement('div');
+        $div.className = 'relative';
+        const $i = document.createElement('i');
+        $i.className = 'fa-solid fa-chevron-left';
+        const $ul = document.createElement('ul');
+        $ul.className = 'row';
+        const $i2 = document.createElement('i');
+        $i2.className = 'fa-solid fa-chevron-right';
 
-// Why wont both queries run?
+        $titles.appendChild($h2);
+        $titles.appendChild($div);
+        $div.appendChild($i);
+
+        for (const title of genreID.data) {
+          const $titleImage = renderTitles(title);
+          $ul.appendChild($titleImage);
+        }
+        $div.appendChild($ul);
+        $div.appendChild($i2);
+        // console.log('genreID', genreID);
+        await delay(1000); // 1s
+      }
+    }
+    runPromises();
+  });
+  xhr.send();
+}
+
+getGenreId();
