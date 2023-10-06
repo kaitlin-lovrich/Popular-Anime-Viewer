3e041719951ce2ffa7939dfd88225b9f5338ec7f
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-06 21:38:33 +0000

    work in progress

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
 const $titles = document.querySelector('#titles');
+// D for desktop lines 3-6
+// const $homeD = document.querySelector('#home');
+const $tvShowsD = document.querySelector('#tv-shows');
+const $moviesD = document.querySelector('#movies');
+// const $searchD = document.querySelector('#search');
+// M for mobile 7-10
+// const $homeM = document.querySelector('.home');
+const $tvShowsM = document.querySelector('.tv-shows');
+const $moviesM = document.querySelector('.movies');
+// const $searchM = document.querySelector('.search');
+// console.log('home', $homeD.tagName, $homeM.tagName);
 
-function renderTitles(title) {
+function renderRow(genreName, data) {
+  const $h2 = document.createElement('h2');
+  $h2.textContent = genreName.toUpperCase();
+  const $div = document.createElement('div');
+  $div.className = 'relative';
+  const $i = document.createElement('i');
+  $i.className = 'fa-solid fa-chevron-left';
+  const $ul = document.createElement('ul');
+  $ul.className = 'row';
+  const $i2 = document.createElement('i');
+  $i2.className = 'fa-solid fa-chevron-right';
+
+  $titles.appendChild($h2);
+  $titles.appendChild($div);
+  $div.appendChild($i);
+  for (const title of data) {
+    const $titleImage = renderTitleImages(title);
+    $ul.appendChild($titleImage);
+  }
+  $div.appendChild($ul);
+  $div.appendChild($i2);
+}
+
+function renderTitleImages(title) {
   const $li = document.createElement('li');
   const $div = document.createElement('div');
   $div.className = 'relative';
@@ -15,77 +49,43 @@ function renderTitles(title) {
   return $li;
 }
 
-function populateRow(urlQuery, genreOrCategoryName) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', urlQuery);
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    const $h2 = document.createElement('h2');
-    $h2.textContent = genreOrCategoryName.toUpperCase();
-    const $div = document.createElement('div');
-    $div.className = 'relative';
-    const $i = document.createElement('i');
-    $i.className = 'fa-solid fa-chevron-left';
-    const $ul = document.createElement('ul');
-    $ul.className = 'row';
-    const $i2 = document.createElement('i');
-    $i2.className = 'fa-solid fa-chevron-right';
-
-    $titles.appendChild($h2);
-    $titles.appendChild($div);
-    $div.appendChild($i);
-    for (const title of xhr.response.data) {
-      const $titleImage = renderTitles(title);
-      $ul.appendChild($titleImage);
-    }
-    $div.appendChild($ul);
-    $div.appendChild($i2);
-  });
-  xhr.send();
-}
-
-populateRow('https://api.jikan.moe/v4/anime?order_by=popularity', 'Popular');
-
-function getGenreId() {
+function getGenreId(category) {
+  // Gets an array of objects containing genre-id's from the Jikan API
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.jikan.moe/v4/genres/anime?filter=genres');
   xhr.responseType = 'json';
   xhr.addEventListener('load', async function () {
+    // Delays the function from running all at once,
+    // instead will run every <specified> miliseconds (ms)
     function delayGetGenreID(ms) {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
+    // Uses the Jikan API endpoint to search for <interpolated id>
     const getGenre = id => {
-      return fetch(
-        `https://api.jikan.moe/v4/anime?order_by=popularity&genres=${id}`
-      );
+      if (category === 'tv-shows') {
+        return fetch(
+          `https://api.jikan.moe/v4/anime?order_by=popularity&type=tv&genres=${id}`
+        );
+      } else if (category === 'movies') {
+        return fetch(
+          `https://api.jikan.moe/v4/anime?order_by=popularity&type=movie&genres=${id}`
+        );
+      } else {
+        return fetch(
+          `https://api.jikan.moe/v4/anime?order_by=popularity&genres=${id}`
+        );
+      }
     };
+    // Promise function that will run when the promise is met
+    // runPromises will generate a DOM tree for each genre of the
+    // array of genres returned from getGenre function call
     async function runPromises() {
       for (const genre of xhr.response.data) {
         const response = await getGenre(genre.mal_id);
-        const genreID = await response.json();
+        const endpointGenreId = await response.json();
 
-        const $h2 = document.createElement('h2');
-        $h2.textContent = genre.name.toUpperCase();
-        const $div = document.createElement('div');
-        $div.className = 'relative';
-        const $i = document.createElement('i');
-        $i.className = 'fa-solid fa-chevron-left';
-        const $ul = document.createElement('ul');
-        $ul.className = 'row';
-        const $i2 = document.createElement('i');
-        $i2.className = 'fa-solid fa-chevron-right';
-
-        $titles.appendChild($h2);
-        $titles.appendChild($div);
-        $div.appendChild($i);
-
-        for (const title of genreID.data) {
-          const $titleImage = renderTitles(title);
-          $ul.appendChild($titleImage);
-        }
-        $div.appendChild($ul);
-        $div.appendChild($i2);
-        await delayGetGenreID(1000); // 1s
+        renderRow(genre.name, endpointGenreId.data);
+        await delayGetGenreID(1000); // .7s
       }
     }
     runPromises();
@@ -93,4 +93,36 @@ function getGenreId() {
   xhr.send();
 }
 
-getGenreId();
+// Default page population
+// getGenreId();
+// document.addEventListener('DOMContentLoaded', getGenreId);
+
+// filter the page to only show tv or movie categories
+function fiterTvMovies(event) {
+  // event.target is either tv or movie category button
+  const $h2 = document.querySelector('h2');
+  const $img = document.querySelector('img');
+  if (event.target === $tvShowsD || event.target === $tvShowsM) {
+    if ($h2 === null) {
+      getGenreId('tv-shows');
+    }
+  } else if (event.target === $moviesD || event.target === $moviesM) {
+    if ($h2 === null) {
+      getGenreId('movies');
+    } else if ($img.baseURI.includes('index.html#tv-shows')) {
+      $titles.childNodes.remove();
+      getGenreId('movies');
+    }
+
+    // how to clear data thats currently on page if !== movies
+  }
+  // console.dir($titles);
+}
+// Populates the homepage with only the TV shows categories when clicked
+$tvShowsD.addEventListener('click', fiterTvMovies);
+$tvShowsM.addEventListener('click', fiterTvMovies);
+// Populates the homepage with only the movie categories when clicked
+$moviesD.addEventListener('click', fiterTvMovies);
+$moviesM.addEventListener('click', fiterTvMovies);
+
+// ill need a 'DOMContentLoaded' event listener on the document object to populate the page when it gets refreshed with the appropriate data category, all, tv shows or movies, whichever $button's class is 'active'
