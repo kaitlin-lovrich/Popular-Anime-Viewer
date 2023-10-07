6342f220755951c7dd7776d332a4aeb64f5c35cf
Kaitlin Lovrich <kaylovrich@gmail.com>
2023-10-07 03:59:01 +0000

    Added event listeners to 'home' 'tv shows' and 'movies' buttons to show filtered views. Buttons lighten when 'activated'. Added local data storage to store last page view and DOMContentLoaded handler to handle page refreshes.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,15 @@
 /* exported data */
+let data = {
+  lastView: ''
+};
+
+function stringifyData(event) {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storrage', dataJSON);
+}
+window.addEventListener('beforeunload', stringifyData);
+
+const previousDataJSON = localStorage.getItem('javascript-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 const $titles = document.querySelector('#titles');
 // D for desktop lines 3-6
-// const $homeD = document.querySelector('#home');
+const $homeD = document.querySelector('#home');
 const $tvShowsD = document.querySelector('#tv-shows');
 const $moviesD = document.querySelector('#movies');
 // const $searchD = document.querySelector('#search');
 // M for mobile 7-10
-// const $homeM = document.querySelector('.home');
+const $homeM = document.querySelector('.home');
 const $tvShowsM = document.querySelector('.tv-shows');
 const $moviesM = document.querySelector('.movies');
 // const $searchM = document.querySelector('.search');
-// console.log('home', $homeD.tagName, $homeM.tagName);
 
+// Creates DOM tree for each <ul> row and appends the title images
 function renderRow(genreName, data) {
   const $h2 = document.createElement('h2');
   $h2.textContent = genreName.toUpperCase();
@@ -34,6 +34,7 @@ function renderRow(genreName, data) {
   $div.appendChild($i2);
 }
 
+// Creates DOM tree for the title images
 function renderTitleImages(title) {
   const $li = document.createElement('li');
   const $div = document.createElement('div');
@@ -49,8 +50,8 @@ function renderTitleImages(title) {
   return $li;
 }
 
+// Gets an array of objects containing genre-id's from the Jikan API
 function getGenreId(category) {
-  // Gets an array of objects containing genre-id's from the Jikan API
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://api.jikan.moe/v4/genres/anime?filter=genres');
   xhr.responseType = 'json';
@@ -61,16 +62,38 @@ function getGenreId(category) {
       return new Promise(resolve => setTimeout(resolve, ms));
     }
     // Uses the Jikan API endpoint to search for <interpolated id>
+    // Update page view data and sets activated button
     const getGenre = id => {
       if (category === 'tv-shows') {
+        data.lastView = 'tv-shows';
+        $tvShowsD.className = 'active';
+        $tvShowsM.className = 'tv-shows active';
+        $moviesD.className = '';
+        $moviesM.className = 'movies';
+        $homeD.className = '';
+        $homeM.className = 'home';
         return fetch(
           `https://api.jikan.moe/v4/anime?order_by=popularity&type=tv&genres=${id}`
         );
       } else if (category === 'movies') {
+        data.lastView = 'movies';
+        $moviesD.className = 'active';
+        $moviesM.className = 'movies active';
+        $tvShowsD.className = '';
+        $tvShowsM.className = 'tv-shows';
+        $homeD.className = '';
+        $homeM.className = 'home';
         return fetch(
           `https://api.jikan.moe/v4/anime?order_by=popularity&type=movie&genres=${id}`
         );
       } else {
+        data.lastView = 'home';
+        $homeD.className = 'active';
+        $homeM.className = 'home active';
+        $tvShowsD.className = '';
+        $tvShowsM.className = 'tv-shows';
+        $moviesD.className = '';
+        $moviesM.className = 'movies';
         return fetch(
           `https://api.jikan.moe/v4/anime?order_by=popularity&genres=${id}`
         );
@@ -85,7 +108,7 @@ function getGenreId(category) {
         const endpointGenreId = await response.json();
 
         renderRow(genre.name, endpointGenreId.data);
-        await delayGetGenreID(1000); // .7s
+        await delayGetGenreID(1000); // 1s
       }
     }
     runPromises();
@@ -93,36 +116,49 @@ function getGenreId(category) {
   xhr.send();
 }
 
-// Default page population
-// getGenreId();
-// document.addEventListener('DOMContentLoaded', getGenreId);
-
-// filter the page to only show tv or movie categories
-function fiterTvMovies(event) {
-  // event.target is either tv or movie category button
-  const $h2 = document.querySelector('h2');
-  const $img = document.querySelector('img');
-  if (event.target === $tvShowsD || event.target === $tvShowsM) {
-    if ($h2 === null) {
-      getGenreId('tv-shows');
-    }
-  } else if (event.target === $moviesD || event.target === $moviesM) {
-    if ($h2 === null) {
-      getGenreId('movies');
-    } else if ($img.baseURI.includes('index.html#tv-shows')) {
-      $titles.childNodes.remove();
-      getGenreId('movies');
-    }
-
-    // how to clear data thats currently on page if !== movies
+// Uses previous view data to keep user on same page if window is refreshed
+document.addEventListener('DOMContentLoaded', function () {
+  if (data.lastView === 'home') {
+    showAllTitles();
+  } else if (data.lastView === 'tv-shows') {
+    fiterTv();
+  } else if (data.lastView === 'movies') {
+    filterMovies();
+  } else {
+    showAllTitles();
   }
-  // console.dir($titles);
-}
-// Populates the homepage with only the TV shows categories when clicked
-$tvShowsD.addEventListener('click', fiterTvMovies);
-$tvShowsM.addEventListener('click', fiterTvMovies);
-// Populates the homepage with only the movie categories when clicked
-$moviesD.addEventListener('click', fiterTvMovies);
-$moviesM.addEventListener('click', fiterTvMovies);
+});
 
-// ill need a 'DOMContentLoaded' event listener on the document object to populate the page when it gets refreshed with the appropriate data category, all, tv shows or movies, whichever $button's class is 'active'
+// Populates the homepage with both the TV shows and Movies when clicked
+$homeD.addEventListener('click', showAllTitles);
+$homeM.addEventListener('click', showAllTitles);
+function showAllTitles() {
+  if (data.lastView !== 'home') {
+    window.stop();
+    $titles.innerHTML = '';
+    getGenreId();
+  }
+}
+
+// Populates the homepage with only the TV shows when clicked
+$tvShowsD.addEventListener('click', fiterTv);
+$tvShowsM.addEventListener('click', fiterTv);
+function fiterTv() {
+  if (data.lastView !== 'tv-shows') {
+    window.stop();
+    $titles.innerHTML = '';
+    getGenreId('tv-shows');
+  }
+}
+
+// Populates the homepage with only the movies when clicked
+$moviesD.addEventListener('click', filterMovies);
+$moviesM.addEventListener('click', filterMovies);
+function filterMovies() {
+  if (data.lastView !== 'movies') {
+    window.stop();
+    $titles.innerHTML = '';
+    getGenreId('movies');
+    data.lastView = 'movies';
+  }
+}
